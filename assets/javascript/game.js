27c126bf9b68e5cd7da3fa212a6595835290b239
Kathleen Catlett <kathleencatlett@comcast.net>
2016-11-26 13:20:51 -0500

    streamlined javascript still does not work after first round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-
-
-
 var gemArr = ["gemA", "gemB", "gemC", "gemD"]
 var imgstring = "";
 var neededScore = 1;  //set this to 1 so the game will not think you won on inital pass
@@ -13,54 +10,45 @@ var i = 0 // loop counter
  $("#winCount").text(winNumber);
  $("#lostCount").text(lostNumber);
 
-//initial variable load
 
-   for (i=0; i<gemArr.length; i++) {
-  		var gemBtn = $("<button>");
-  		gemBtn.addClass("bttn");
-  		gemBtn.attr("data-gem", Math.floor((Math.random() * 12) + 1));
-  		imgstring = "url(assets/images/gem" + i + ".png)";
-  		gemBtn.css("background-image", imgstring);
-      $("#buttons").append(gemBtn);
-}
+ // call setvar to set up crystals & scores
+$(document).ready(function(){
 
-  neededScore = Math.floor((Math.random() * 120) + 19);
-  $("#currentscore").text(neededScore);
+var x = setvar()  
 
-// create case statement (Or javascript version)
-// check to see if = needed score user won winNumber++
-// if > neededScore user lost - lostNumber++
-// if < needScore keep playing
 
-  //   $(".bttn").on("click", function() {
-  //       console.log("clicked");
-  //       var gemValue = $("<div>");            
-  //       userScore = userScore + $(this).data("gem");
-  //       $("#userScoreDisp").text("current score = " + userScore);
-  //       if (userScore > neededScore) {
-  //         alert("you lost")
-  //         lostNumber++
-  //         $("#lostCount").text(lostNumber);
-  //         resetvar("lost") }
-  //       if (userScore == neededScore){
-  //         alert("you won")
-  //         winNumber++
-  //         $("#winCount").text(winNumber);
-  //         resetvar("won")
-  // }
-  // })  // end of on click function
 
-  function resetvar(winorlose) {
+
+  $(".bttn").on("click", function() {
+        console.log("clicked");
+        var gemValue = $("<div>");            
+        userScore = userScore + $(this).data("gem");
+        $("#userScoreDisp").text("current score = " + userScore);
+        if (userScore > neededScore) {
+          alert("you lost");
+          lostNumber++;
+          $("#lostCount").text(lostNumber);
+          x = setvar();
+           }
+        else if (userScore == neededScore){
+          alert("you won");
+          winNumber++;
+          $("#winCount").text(winNumber);
+          x = setvar();
+         }
+  })  //end of on click
+
+})
+
+
+// can probably streamline this a bit
+
+function setvar() {
     console.log("in resetvar function")
     userScore = 0;
     $("#userScoreDisp").text("current score = " + userScore);
-     $("#buttons").empty()
-  for (i=0; i<gemArr.length; i++) {
-      // var gemBtn = $("<button>");
-      // // var gemBtn = $("<button>");
-      // gemBtn.attr("data-gem", Math.floor((Math.random() * 12) + 1));
-      // $("#buttons").replaceWith(gemBtn);
-
+    $("#buttons").empty();  //remove initial genms or it will duplicate
+    for (i=0; i<gemArr.length; i++) {
       console.log("loop #" + i);
       gemBtn = $("<button>");
       gemBtn.addClass("bttn");
@@ -73,36 +61,7 @@ var i = 0 // loop counter
       neededScore = Math.floor((Math.random() * 120) + 19);
       $("#currentscore").text(neededScore);
 
-  }  // end of resetvar function
-
-
-
-  $(".bttn").on("click", function() {
-        console.log("clicked");
-        var gemValue = $("<div>");            
-        userScore = userScore + $(this).data("gem");
-        $("#userScoreDisp").text("current score = " + userScore);
-        if (userScore > neededScore) {
-          alert("you lost")
-          lostNumber++
-          $("#lostCount").text(lostNumber);
-          resetvar("lost")
-           }
-        if (userScore == neededScore){
-          alert("you won")
-          winNumber++
-          $("#winCount").text(winNumber);
-          resetvar("won")
-         }
-
-  })  //end of on click
-
-
-    //check to see if won/lost/continue
-    //if won increment won counter
-    //if lost increment lost counter
-    // if won or lost reset gem values & needed score
-
-    //if not won or lost just leave everything alone
+  }  // end of setvar function
+    
 
 
