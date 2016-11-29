ee8d5c5ca2bb773b550190c96897b0841a1d5eb2
Kathleen Catlett <kathleencatlett@comcast.net>
2016-11-29 09:31:42 -0500

    logic done just a few more layout adjustments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,58 +10,51 @@ var i = 0 // loop counter
  $("#winCount").text(winNumber);
  $("#lostCount").text(lostNumber);
 
-
- // call setvar to set up crystals & scores
-$(document).ready(function(){
-
 var x = setvar()  
 
 
+$(document).on('click', ".bttn", function() {
 
-
-  $(".bttn").on("click", function() {
-        console.log("clicked");
         var gemValue = $("<div>");            
         userScore = userScore + $(this).data("gem");
-        $("#userScoreDisp").text("current score = " + userScore);
+        $("#userScoreDisp").text(userScore);
+
         if (userScore > neededScore) {
-          alert("you lost");
+          alert("Sorry, You Lost");
           lostNumber++;
           $("#lostCount").text(lostNumber);
           x = setvar();
            }
+
         else if (userScore == neededScore){
-          alert("you won");
+          alert("Congratulations! You Won");
           winNumber++;
           $("#winCount").text(winNumber);
           x = setvar();
          }
-  })  //end of on click
+  })  //end of on click function
 
-})
-
-
-// can probably streamline this a bit
 
 function setvar() {
-    console.log("in resetvar function")
+    console.log("in setvar function")
+    
     userScore = 0;
-    $("#userScoreDisp").text("current score = " + userScore);
+    $("#userScoreDisp").text(userScore);
     $("#buttons").empty();  //remove initial genms or it will duplicate
+    
     for (i=0; i<gemArr.length; i++) {
       console.log("loop #" + i);
       gemBtn = $("<button>");
       gemBtn.addClass("bttn");
       gemBtn.attr("data-gem", Math.floor((Math.random() * 12) + 1));
-      imgstring = "url(assets/images/gem" + i + ".png)";
-      gemBtn.css("background-image", imgstring);
+      gemBtn.css("background-image", "url(assets/images/gem" + i + ".png)");
        $("#buttons").append(gemBtn);
     }  //end of for loop
-
+    
       neededScore = Math.floor((Math.random() * 120) + 19);
       $("#currentscore").text(neededScore);
-
-  }  // end of setvar function
+      console.log("end of setvar function");
+}  // end of setvar function
     
 
 
