8788d1182568f1c9180d51e278fa398f1abeff84
Kathleen Catlett <kathleencatlett@comcast.net>
2016-11-30 16:06:28 -0500

    Now I think I am really done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var neededScore = 1;  //set this to 1 so the game will not think you won on init
 var userScore = 0;
 var winNumber = 0;
 var lostNumber = 0;
+var clickcount = 0;
 
 var i = 0 // loop counter
 
@@ -13,20 +14,25 @@ var i = 0 // loop counter
 var x = setvar()  
 
 $(document).on('click', ".bttn", function() {
-            
+    clickcount++
+    if(clickcount == 1){
+      // clear the won/lost 
+      $("#wonorLostU").html("");
+      $("#wonorlost").html("");
+    }        
     userScore = userScore + Number($(this).attr("data-gem"));
     $("#userScoreDisp").text(userScore);
 
     if (userScore > neededScore) {
-      // $("#wonorlostU").html("<h4>You</h4>")
-      //  $("#wonorlost").html("<h4>Lost</h4>")
+      $("#wonorLostU").html("You");
+      $("#wonorlost").html("Lost!");
       lostNumber++;
       $("#lostCount").text(lostNumber);
       x = resetvar();
     }
     else if (userScore == neededScore){
-          // $("#wonorlostU").html("<h4>You</h4>")
-          // $("#wonorlost").html("<h4>Won!</h4>")
+      $("#wonorLostU").html("You");
+      $("#wonorlost").html("Won!");
       winNumber++;
       $("#winCount").text(winNumber);
       x = resetvar();
@@ -57,12 +63,11 @@ function resetvar(){
   for (i=0; i<gemArr.length; i++) {
     $(".bttn").eq(i).attr("data-gem", Math.floor((Math.random() * 12) + 1));
   }
-      // reset score variables
-      neededScore = Math.floor((Math.random() * 120) + 19);
-      $("#currentscore").text(neededScore);
-      userScore = 0;
-       $("#userScoreDisp").text(userScore);
-      // $("#wonorlost").html("");
-      // $("#wonorlostU").html("");
+    // reset score variables
+    neededScore = Math.floor((Math.random() * 120) + 19);
+    $("#currentscore").text(neededScore);
+    userScore = 0;
+    $("#userScoreDisp").text(userScore);
+    clickcount = 0
 }  // end of resetvar
 // }
